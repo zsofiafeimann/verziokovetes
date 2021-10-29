afa78c77caadeb985e34efb87642284edb264f82
Zsófia Irma Feimann <zsofia.feimann@stud.uni-corvinus.hu>
2021-10-29 10:30:45 +0200

    Load Xml

--- WebService/WebService/Form1.cs
+++ WebService/WebService/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using WebService.Entities;
 using WebService.MnbServiceReference;
 
@@ -21,12 +22,43 @@ namespace WebService
         {
             InitializeComponent();
 
-            GetExchangeRates();
+            string xmlsting = GetExchangeRates();
+
+            LoadXml(xmlsting);
 
             dataGridView1.DataSource = Rates;
         }
 
-        private void GetExchangeRates()
+        private void LoadXml(string input)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(input);
+
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                RateData r = new RateData();
+
+                r.Date = DateTime.Parse(item.GetAttribute("date"));
+
+                XmlElement child = (XmlElement)item.FirstChild;
+
+                r.Currency = child.GetAttribute("curr");
+
+                r.Value = decimal.Parse(child.InnerText);
+
+                int unit = int.Parse(child.GetAttribute("unit"));
+
+                if (unit!=0)
+                {
+                    r.Value = r.Value / unit;
+                  
+                }
+
+                Rates.Add(r);
+            }
+        }
+
+        string GetExchangeRates()
         {
             MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
 
@@ -36,11 +68,11 @@ namespace WebService
             request.startDate = "2020-01-01";
             request.endDate = "2020-06-30";
 
-            mnbService.GetExchangeRates(request);
+            //mnbService.GetExchangeRates(request);
 
             var response = mnbService.GetExchangeRates(request);
             string result = response.GetExchangeRatesResult;
-            File.WriteAllText("export.xml", result);
+            return result;
         }
 
 
