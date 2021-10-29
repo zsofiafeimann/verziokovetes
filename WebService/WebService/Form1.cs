012464e833934051d0bb8234a9211b2f4b941a08
Zsófia Irma Feimann <zsofia.feimann@stud.uni-corvinus.hu>
2021-10-29 10:12:32 +0200

    Get Webservies

--- WebService/WebService/Form1.cs
+++ WebService/WebService/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WebService.MnbServiceReference;
 
 namespace WebService
 {
@@ -15,6 +17,27 @@ namespace WebService
         public Form1()
         {
             InitializeComponent();
+
+            GetExchangeRates();
         }
+
+        private void GetExchangeRates()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+
+            request.currencyNames = "EUR";
+            request.startDate = "2020-01-01";
+            request.endDate = "2020-06-30";
+
+            mnbService.GetExchangeRates(request);
+
+            var response = mnbService.GetExchangeRates(request);
+            string result = response.GetExchangeRatesResult;
+            File.WriteAllText("export.xml", result);
+        }
+
+
     }
 }
