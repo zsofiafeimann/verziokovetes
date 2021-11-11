5d7feb64320b28d396125fe4c518552b0a4c26a4
Zsófia Irma Feimann <zsofia.feimann@stud.uni-corvinus.hu>
2021-11-11 11:10:15 +0100

    Create BallFactory

---
+++ FejlesztésiMintak/FejlesztésiMintak/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FejlesztésiMintak.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
