24acf0e652d9c428cbedc898b83894b545f55358
Zsófia Irma Feimann <zsofia.feimann@stud.uni-corvinus.hu>
2021-11-12 21:11:43 +0100

    Create BallFactory

--- JatekGyar/JatekGyar/Form1.cs
+++ JatekGyar/JatekGyar/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JatekGyar.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,15 @@ namespace JatekGyar
 {
     public partial class Form1 : Form
     {
+
+
+
         public Form1()
         {
             InitializeComponent();
+            
+
+            
         }
     }
 }
