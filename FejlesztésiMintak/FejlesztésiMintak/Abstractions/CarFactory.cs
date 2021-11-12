1219df8838822065e90a1c663c69ffad1a479a8a
Zsófia Irma Feimann <zsofia.feimann@stud.uni-corvinus.hu>
2021-11-12 21:06:39 +0100

    Create Ball

---
+++ FejlesztésiMintak/FejlesztésiMintak/Abstractions/CarFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FejlesztésiMintak.Abstractions
+{
+    public interface CarFactory
+    {
+    public Toy CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
+}
