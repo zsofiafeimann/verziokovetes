05f13668f01e7d2233d6458cfb2c42481b4fb417
Zsófia Irma Feimann <zsofia.feimann@stud.uni-corvinus.hu>
2021-11-26 10:31:13 +0100

    Valid email

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // Arrange
+
+            var accountController = new AccountController();
+
+            //Act
+
+            var result = accountController.ValidateEmail(email);
+
+            //Assert
+
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
