a7a383060f8e979d1af02345dc6a7e0501abcc0d
Zsófia Irma Feimann <zsofia.feimann@stud.uni-corvinus.hu>
2021-12-03 10:07:52 +0100

    Create Classes

---
+++ Mikroszimulacio/Mikroszimulacio/Entities/BirthProbability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimulacio.Entities
+{
+    public class BirthProbability
+    {
+        public int BirthYear { get; set; }
+        public int NbrOfChildren { get; set; }
+
+        public double P { get; set; }
+
+
+    }
+}
---
+++ Mikroszimulacio/Mikroszimulacio/Entities/DeathProbability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimulacio.Entities
+{
+    public class DeathProbability
+    {
+        public int Age { get; set; }
+
+        public Gender Gender { get; set; }
+        public double P { get; set; }
+
+
+    }
+}
