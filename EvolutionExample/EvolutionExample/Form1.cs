b95b36af404c17b142c1a06d94f316623754e592
Zsófia Irma Feimann <zsofia.feimann@stud.uni-corvinus.hu>
2021-12-10 10:19:32 +0100

    PopulationSize

--- EvolutionExample/EvolutionExample/Form1.cs
+++ EvolutionExample/EvolutionExample/Form1.cs
@@ -16,6 +16,12 @@ namespace EvolutionExample
         GameController gc = new GameController();
 
         GameArea ga = null;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
@@ -23,8 +29,12 @@ namespace EvolutionExample
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
-            gc.AddPlayer();
-            gc.Start(true);
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            
+            gc.Start();
         }
     }
 }
